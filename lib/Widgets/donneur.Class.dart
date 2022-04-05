ad7b2e6e58c3cf41f1411441184761b7db5a6291
Sen-Yam <96739035+Sen-Yam@users.noreply.github.com>
2022-04-05 12:40:56 +0000

    Create donneur.Class.dart

---
+++ lib/Widgets/donneur.Class.dart
@@ -0,0 +1,9 @@
+class Person{
+ final  String name;
+   final String adr;
+  final  String phone ;
+  final  String sadaka;
+  final String info;
+
+  Person(this.name, this.adr , this.phone , this.sadaka , this.info);
+}
