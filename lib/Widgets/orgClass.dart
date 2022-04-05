8586d05671c8dfc8bc485ac81742c550cb640507
Sen-Yam <96739035+Sen-Yam@users.noreply.github.com>
2022-04-05 12:38:18 +0000

    Create orgClass.dart

---
+++ lib/Widgets/orgClass.dart
@@ -0,0 +1,10 @@
+class Organizer {
+    final String org_name;
+    final String phone_organization;
+    final String services;
+    final  GeoPoint location ;
+
+    Organizer( this.org_name,this.phone_organization,this.services, this.location);
+
+
+}
