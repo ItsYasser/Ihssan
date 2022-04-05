06a03a38962c3279c3d45161e1aa402c9f4270c5
Sen-Yam <96739035+Sen-Yam@users.noreply.github.com>
2022-04-05 12:45:21 +0000

    Update orgClass.dart

--- lib/Widgets/orgClass.dart
+++ lib/Widgets/orgClass.dart
@@ -1,8 +1,8 @@
 class Organizer {
-    final String org_name;
-    final String phone_organization;
-    final String services;
-    final  GeoPoint location ;
+     String org_name;
+     String phone_organization;
+     String services;
+      GeoPoint location ;
 
     Organizer( this.org_name,this.phone_organization,this.services, this.location);
 
