336a6eb278ba102c39d292f42e5d6eaeca34c8b3
Yasser Harzelli <gmysto6@gmail.com>
2022-04-05 15:32:41 +0100

    merge

--- lib/Widgets/orgClass.dart
+++ lib/Widgets/orgClass.dart
@@ -1,10 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Organizer {
-     String org_name;
-     String phone_organization;
-     String services;
-      GeoPoint location ;
+  String org_name;
+  String phone_organization;
+  String services;
+  GeoPoint location;
 
-    Organizer( this.org_name,this.phone_organization,this.services, this.location);
-
-
+  Organizer(
+      this.org_name, this.phone_organization, this.services, this.location);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_festival/Util/constants.dart';
 
@@ -9,7 +10,13 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'View/on_boarding.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
